80e477dd6299026319291489072c327f2b1fde58
Tal Etinger <etingertal@gmail.com>
2020-09-16 01:30:18 +0300

    Jenkinsfile fixes

--- pipeline/Jenkinsfile.groovy
+++ pipeline/Jenkinsfile.groovy
@@ -24,22 +24,22 @@ pipeline {
             }
         }
 
-//        stage ('compile') {
-//            steps {
-//                sh 'mvn -DskipTests package' // Using package for later use by docker
-//            }
-//        }
-//
-//        stage ('test') {
-//            steps {
-//                sh 'mvn -Dmaven.test.failure.ignore=true test'
-//            }
-//            post {
-//                success {
-//                    junit 'target/surefire-reports/**/*.xml'
-//                }
-//            }
-//        }
+        stage ('compile') {
+            steps {
+                sh 'mvn -DskipTests package' // Using package for later use by docker
+            }
+        }
+
+        stage ('test') {
+            steps {
+                sh 'mvn -Dmaven.test.failure.ignore=true test'
+            }
+            post {
+                success {
+                    junit 'target/surefire-reports/**/*.xml'
+                }
+            }
+        }
 
         stage ('build docker image') {
             steps {
