f40b9b83112b86c51b606b82853b716166898fc6
Tal Etinger <etingertal@gmail.com>
2020-09-15 23:40:17 +0300

    Added: README, Dockerfile, Jenkinsfile

---
+++ pipeline/Jenkinsfile.groovy
@@ -0,0 +1,48 @@
+pipeline {
+    agent any
+    tools {
+        maven 'Maven 3.3.9'
+        jdk 'jdk11'
+    }
+    stages {
+        stage ('init') {
+            steps {
+                pom = readMavenPom file: 'pom.xml'
+                sh '''
+                    echo APPLICATION=${pom.artifactId}:${pom.version}
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                '''
+            }
+        }
+
+        stage ('compile') {
+            steps {
+                sh 'mvn -DskipTests package' // Using package for later use by docker
+            }
+        }
+
+        stage ('test') {
+            steps {
+                sh 'mvn -Dmaven.test.failure.ignore=true test'
+            }
+            post {
+                success {
+                    junit 'target/surefire-reports/**/*.xml'
+                }
+            }
+        }
+
+        stage ('build docker image') {
+            steps {
+                sh 'docker build -t etingertal/jf-exercise .'
+            }
+        }
+
+        stage ('push docker image') {
+            steps {
+                sh 'docker push etingertal/jf-exercise'
+            }
+        }
+    }
+}
